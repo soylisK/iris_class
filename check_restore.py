6f90eb0e0dba61457c50bc803a774621c01bfc6a
soylisK <soylis.k@gmail.com>
2019-05-19 00:12:15 +0300

    upd..

--- check_restore.py
+++ check_restore.py
@@ -6,6 +6,6 @@ import os
 
 
 with tf.Session() as sess:
-  new_saver = tf.train.import_meta_graph('my_test_model-1000.meta')
+  new_saver = tf.train.import_meta_graph('model.ckpt.meta')
   new_saver.restore(sess, tf.train.latest_checkpoint('./'))
   print(sess.run('W1:0'))
