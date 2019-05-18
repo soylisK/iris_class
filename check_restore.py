a7c35a7cf1b211c45676371afb39a92f390444e4
soylisK <soylis.k@gmail.com>
2019-05-19 00:11:32 +0300

    restore..upload

---
+++ check_restore.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+
+
+
+with tf.Session() as sess:
+  new_saver = tf.train.import_meta_graph('my_test_model-1000.meta')
+  new_saver.restore(sess, tf.train.latest_checkpoint('./'))
+  print(sess.run('W1:0'))
